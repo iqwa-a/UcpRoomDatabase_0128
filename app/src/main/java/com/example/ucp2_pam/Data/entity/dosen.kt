6c55780258c802e62e5d963f59e590427cba8ed4
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-16 17:22:09 +0700

    membuat entity dosen

---
+++ app/src/main/java/com/example/ucp2_pam/Data/entity/dosen.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2_pam.Data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Menandai kelas ini sebagai entitas database untuk tabel "dosen"
+@Entity(tableName = "dosen")
+data class dosen(
+    @PrimaryKey // Menandai kolom Nidn sebagai primary key
+    val Nidn: String, // Nomor Induk Mahasiswa, unik untuk setiap mahasiswa
+    val Nama: String, // Nama mahasiswa
+    val jenisKelamin: String, // Jenis kelamin mahasiswa
+)
