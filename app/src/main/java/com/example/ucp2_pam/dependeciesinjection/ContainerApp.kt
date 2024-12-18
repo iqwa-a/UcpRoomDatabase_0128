de890fcf9bf74ca1468351d1595bbcc93717f133
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-18 23:57:55 +0700

    interface InterfaceContainerApp

--- app/src/main/java/com/example/ucp2_pam/dependeciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2_pam/dependeciesinjection/ContainerApp.kt
@@ -2,6 +2,10 @@ package com.example.ucp2_pam.dependeciesinjection
 
 import android.content.Context
 
+
+interface InterfaceContainerApp {
+    val repositoryMatakuliah : RepositoryMatakuliah
+}
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
     override val repositoryMatakuliah: RepositoryMatakuliah by lazy {
         LocalRepositoryMatakuliah(DosenDatabase.getDatabase(context).matakuliahDao())
