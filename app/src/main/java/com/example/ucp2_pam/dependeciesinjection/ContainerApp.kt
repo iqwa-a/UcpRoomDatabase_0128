68703e2f3de677f981ac7b5ac76e97eb632b86aa
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-20 22:16:14 +0700

    Implementasi InterfaceContainerApp Matakuliah

--- app/src/main/java/com/example/ucp2_pam/dependeciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2_pam/dependeciesinjection/ContainerApp.kt
@@ -1,13 +1,18 @@
 package com.example.ucp2_pam.dependeciesinjection
 
 import android.content.Context
+import com.example.ucp2_pam.Data.database.KrsDatabase
+import com.example.ucp2_pam.repository.RepositoryMatakuliah
 
 
 interface InterfaceContainerApp {
     val repositoryMatakuliah : RepositoryMatakuliah
+
 }
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
+
     override val repositoryMatakuliah: RepositoryMatakuliah by lazy {
-        LocalRepositoryMatakuliah(DosenDatabase.getDatabase(context).matakuliahDao())
+        LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).matakuliahDao())
     }
+
 }
