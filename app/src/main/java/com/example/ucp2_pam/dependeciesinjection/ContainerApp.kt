7583fb9c77a898aba7ac0b8e03e47ea59f787784
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-20 23:55:10 +0700

    memperbaiki TopAppBar

--- app/src/main/java/com/example/ucp2_pam/dependeciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2_pam/dependeciesinjection/ContainerApp.kt
@@ -2,6 +2,9 @@ package com.example.ucp2_pam.dependeciesinjection
 
 import android.content.Context
 import com.example.ucp2_pam.Data.database.KrsDatabase
+import com.example.ucp2_pam.repository.LocalRepositoryDosen
+import com.example.ucp2_pam.repository.LocalRepositoryMatakuliah
+import com.example.ucp2_pam.repository.RepositoryDosen
 import com.example.ucp2_pam.repository.RepositoryMatakuliah
 
 
@@ -13,6 +16,9 @@ class ContainerApp(private val context: Context) : InterfaceContainerApp {
 
     override val repositoryMatakuliah: RepositoryMatakuliah by lazy {
         LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).matakuliahDao())
+    }
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDatabase(context).dosenDao())
     }
 
 }
