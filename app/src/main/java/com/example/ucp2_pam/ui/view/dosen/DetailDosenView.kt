867d564aa65ebcd310468b44e5421e516379355b
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-21 16:44:06 +0700

    Membuat BodyDetailDosen

--- app/src/main/java/com/example/ucp2_pam/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/dosen/DetailDosenView.kt
@@ -1,15 +1,26 @@
 package com.example.ucp2_pam.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_pam.ui.contumwidget.TopAppBar
 import com.example.ucp2_pam.ui.viewmodel.dosen.DetailDosenViewModel
+import com.example.ucp2_pam.ui.viewmodel.dosen.DetailUiState
 import com.example.ucp2_pam.ui.viewmodel.dosen.PenyediaDosenViewModel
+import com.example.ucp2_pam.ui.viewmodel.dosen.toDosenEntity
 
 @Composable
 fun DetailDosenView(
@@ -33,5 +44,47 @@ fun DetailDosenView(
             modifier = Modifier.padding(innerPadding),
             detailDosenUiState = detailDosenUiState,
         )
+    }
+}
+@Composable
+fun BodyDetailDosen(
+    modifier: Modifier = Modifier,
+    detailDosenUiState: DetailUiState = DetailUiState(),
+) {
+    when {
+        detailDosenUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        detailDosenUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDosen(
+                    dosen = detailDosenUiState.detailUiEvent.toDosenEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+            }
+        }
+
+        detailDosenUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
     }
 }
