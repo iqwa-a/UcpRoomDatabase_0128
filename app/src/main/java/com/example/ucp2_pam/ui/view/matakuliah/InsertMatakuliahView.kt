463c175abf3b8df4e6b500bbfb71d66c23266949
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-21 14:13:36 +0700

    Membuat fun InsertBodyMatakuliah

--- app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/InsertMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/InsertMatakuliahView.kt
@@ -1,17 +1,35 @@
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2_pam.Data.entity.Dosen
 import com.example.ucp2_pam.ui.contumwidget.TopAppBar
+import com.example.ucp2_pam.ui.viewmodel.matakuliiah.FormErrorState
+import com.example.ucp2_pam.ui.viewmodel.matakuliiah.MatakuliahEvent
+import com.example.ucp2_pam.ui.viewmodel.matakuliiah.MatakuliahUIState
 import com.example.ucp2_pam.ui.viewmodel.matakuliiah.MatakuliahViewModel
 import com.example.ucp2_pam.ui.viewmodel.matakuliiah.PenyediaMatakuliahViewModel
 import kotlinx.coroutines.launch
@@ -70,6 +88,39 @@ fun InsertMatakuliahView(
                     onNavigate()
                 }
             )
+        }
+    }
+}
+
+@Composable
+fun InsertBodyMatakuliah(
+    modifier: Modifier = Modifier,
+    onValueChange: (MatakuliahEvent) -> Unit,
+    onClick: () -> Unit,
+    uiState: MatakuliahUIState,
+    listDosen: HomeUiState
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMatakuliah(
+            matakuliahEvent = uiState.matakuliahEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth(),
+            listDsn = listDosen.listDsn
+        )
+        Button(
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF343434), // Warna latar tombol
+                contentColor = Color.White // Warna teks
+            ),
+            modifier = Modifier.fillMaxWidth() // Tombol mengambil lebar penuh
+        ) {
+            Text("Simpan")
         }
     }
 }
