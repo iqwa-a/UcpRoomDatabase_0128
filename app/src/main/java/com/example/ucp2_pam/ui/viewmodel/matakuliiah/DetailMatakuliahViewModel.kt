bc197bdf65149a6c6dc880532f6b2f5540f30d12
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-21 13:03:42 +0700

    Tambahkan DetailMatakuliahViewModel

---
+++ app/src/main/java/com/example/ucp2_pam/ui/viewmodel/matakuliiah/DetailMatakuliahViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.ucp2_pam.ui.viewmodel.matakuliiah
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2_pam.repository.RepositoryMatakuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
+
+class DetailMatakuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMatakuliah: RepositoryMatakuliah,
+) : ViewModel() {
+
+    // Mendapatkan KODE dari SavedStateHandle
+    private val _kode: String = checkNotNull(savedStateHandle[DestinasiMatakuliahDetail.KODE])
+
+    // Mendapatkan detail mahasiswa dan mengelola UI state
+    val detailMatakuliahUiState: StateFlow<DetailMatakuliahUiState> = repositoryMatakuliah.getMatakuliah(_kode)
+        .filterNotNull() // Pastikan data tidak null
+        .map {
+            DetailMatakuliahUiState(
+                detailMatakuliahUiEvent = it.toDetailMatakuliahUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailMatakuliahUiState(isLoading = true)) // Set status loading saat mulai
+            delay(600) // Simulasi delay jika diperlukan
+        }
+        .catch {
+            emit(
+                DetailMatakuliahUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailMatakuliahUiState(
+                isLoading = true,
+            ),
+        )
+
+    // Fungsi untuk menghapus matakuliah
+    fun deleteMatakuliah() {
+        detailMatakuliahUiState.value.detailMatakuliahUiEvent.toMatakuliahEntity().let {
+            viewModelScope.launch {
+                repositoryMatakuliah.deleteMtk(it)
+            }
+        }
+    }
+}
