833dabae71c11f85f305123b9239a553971af32a
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-21 16:06:18 +0700

    Salin kode Tambahkan komponen detail dan dialog

--- app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/DetailMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/DetailMatakuliahView.kt
@@ -4,15 +4,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,7 +25,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_pam.ui.contumwidget.TopAppBar
 import com.example.ucp2_pam.ui.viewmodel.matakuliiah.DetailMatakuliahViewModel
@@ -129,4 +138,107 @@ fun BodyDetailMatakuliah(
             }
         }
     }
+}
+@Composable
+fun ItemDetailMatakuliah(
+    modifier: Modifier = Modifier,
+    matakuliah: Matakuliah
+) {
+    Card(
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            ComponentDetailMatakuliah(judul = "Kode",
+                isinya = matakuliah.kode)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailMatakuliah(judul = "Nama",
+                isinya = matakuliah.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailMatakuliah(judul = "Sks",
+                isinya = matakuliah.sks)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailMatakuliah(judul = "Semester",
+                isinya = matakuliah.semester)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailMatakuliah(judul = "Jenis",
+                isinya = matakuliah.jenis)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailMatakuliah(judul = "DosenPengampu",
+                isinya = matakuliah.dosenpengampu)
+        }
+    }
+}
+@Composable
+fun ComponentDetailMatakuliah(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    //Menambahkan AlertDialog
+    AlertDialog(
+        onDismissRequest = { /* Do nothing */ },
+        title = {
+            Text(
+                text = "Hapus Data",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF343434) // Warna teks judul
+            )
+        },
+        text = {
+            Text(
+                text = "Apakah Anda yakin ingin menghapus data ini?",
+                fontSize = 16.sp,
+                color = Color(0xFF555555) // Warna teks konten
+            )
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .clip(RoundedCornerShape(16.dp)), // Membuat dialog dengan sudut melengkung
+        dismissButton = {
+            TextButton(
+                onClick = onDeleteCancel,
+                colors = ButtonDefaults.textButtonColors(
+                    contentColor = Color(0xFF555555) // Warna biru untuk tombol "Cancel"
+                )
+            ) {
+                Text(text = "Batal")
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onDeleteConfirm,
+                colors = ButtonDefaults.textButtonColors(
+                    contentColor = Color(0xFF555555) // Warna biru untuk tombol "Yes"
+                )
+            ) {
+                Text(text = "Ya")
+            }
+        },
+        containerColor = Color.White, // Warna latar belakang dialog
+        tonalElevation = 6.dp // Efek elevasi untuk bayangan
+    )
 }
