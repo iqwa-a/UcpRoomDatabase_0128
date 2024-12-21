e3f0b2303668fd030a0cde9cfb2eceec447e4fae
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-21 12:13:03 +0700

    Membuat Class HomeMatakuliahViewModel

---
+++ app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/HomeMatakuliahView.kt
@@ -0,0 +1,55 @@
+package com.example.ucp2_pam.ui.view.matakuliah
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.ucp2_pam.ui.contumwidget.TopAppBar
+
+@Composable
+fun HomeMatakuliahView(
+    viewModel: HomeMatakuliahViewModel = viewModel(factory = PenyediaMatakuliahViewModel.Factory),
+    onAddMatakuliah: () -> Unit = {},
+    onBack:()->Unit,
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Matakuliah",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMatakuliah,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Matakuliah"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeMatakuliahUiState.collectAsState()
+
+        BodyHomeMatakuliahView(
+            homeMatakuliahUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
