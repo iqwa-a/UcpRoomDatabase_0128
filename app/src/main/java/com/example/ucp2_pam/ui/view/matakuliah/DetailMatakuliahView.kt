ac73ae1ce84e6196e7c39f33848e3705f82f8fad
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-21 15:08:41 +0700

    Membuat fun BodyDetailMatakuliah

--- app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/DetailMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/DetailMatakuliahView.kt
@@ -1,19 +1,35 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_pam.ui.contumwidget.TopAppBar
+import com.example.ucp2_pam.ui.viewmodel.matakuliiah.DetailMatakuliahViewModel
+import com.example.ucp2_pam.ui.viewmodel.matakuliiah.PenyediaMatakuliahViewModel
 
 @Composable
 fun DetailMatakuliahView(
     modifier: Modifier = Modifier,
-    viewModel: DetailMatakuliahViewModel = viewModel(factory = penyediaMatakuliahViewModel.Factory),
+    viewModel: DetailMatakuliahViewModel = viewModel(factory = PenyediaMatakuliahViewModel.Factory),
     onBack: () -> Unit = {},
     onEditClick: (String) -> Unit = {},
     onDeleteClick: () -> Unit = {}
@@ -51,5 +67,66 @@ fun DetailMatakuliahView(
                 onDeleteClick()
             },
         )
+    }
+}
+@Composable
+fun BodyDetailMatakuliah(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = {}
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() // Tampilkan loading
+            }
+        }
+        detailUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailMatakuliah(
+                    matakuliah = detailUiState.detailUiEvent.toMatakuliahEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty == false -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data Tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
     }
 }
