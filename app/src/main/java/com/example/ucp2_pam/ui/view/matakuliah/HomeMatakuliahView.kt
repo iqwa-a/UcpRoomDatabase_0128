e24cc8627d65aeb784b8654b97170f12260282a6
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-12-21 12:22:10 +0700

    Membuat BodyHomeMatakuliahView

--- app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/HomeMatakuliahView.kt
+++ app/src/main/java/com/example/ucp2_pam/ui/view/matakuliah/HomeMatakuliahView.kt
@@ -1,16 +1,28 @@
 package com.example.ucp2_pam.ui.view.matakuliah
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ucp2_pam.ui.contumwidget.TopAppBar
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeMatakuliahView(
@@ -51,5 +63,62 @@ fun HomeMatakuliahView(
             },
             modifier = Modifier.padding(innerPadding)
         )
+    }
+}
+@Composable
+fun BodyHomeMatakuliahView(
+    homeMatakuliahUiState: HomeMatakuliahUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeMatakuliahUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        homeMatakuliahUiState.isError -> {
+            LaunchedEffect(homeMatakuliahUiState.errorMessage) {
+                homeMatakuliahUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+
+        homeMatakuliahUiState.listMtk.isEmpty() -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data matakuliah.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            ListMatakuliah(
+                listMtk = homeMatakuliahUiState.listMtk,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+
+                },
+                modifier = modifier
+            )
+        }
     }
 }
